5e1e5fc0f275af5eae99703551d2451a00436fe6
kancheng <zxdfgcv@gmail.com>
2017-02-10 15:23:40 +0800

    kan edit

---
+++ rfile/pbpkgs.R
@@ -0,0 +1,18 @@
+﻿local({r = getOption("repos")
+r["CRAN"] = "https://cloud.r-project.org/"
+options(repos=r)})
+
+pkgs = c("devtools", "lattice", "GGally", "lubridate", "stringr", "ggplot2", "scales",
+ "reshape2", "data.table", "coefplot", "broom", "RJDBC", "XLConnect", "RJSONIO", "sp", "png", "pixmap", "mapdata", "maptools", "maps", "rgeos", "Rcmdr")
+
+pkgs = pkgs[!( pkgs %in% installed.packages()[,"Package"] )]
+
+# install
+if(length(pkgs)){
+	install.packages(pkgs)
+}
+
+require(devtools)
+install_github('rCharts', 'ramnathv')
+
+
